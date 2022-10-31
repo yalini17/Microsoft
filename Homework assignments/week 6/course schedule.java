93ab74c9303b4e4f7552fe65073f52db9bdbe90b
yalini17 <113898885+yalini17@users.noreply.github.com>
2022-10-31 14:36:55 +0530

    Add files via upload

---
+++ Homework assignments/week 6/course schedule.java
@@ -0,0 +1,38 @@
+class Solution {
+    public boolean canFinish(int numCourses, int[][] prerequisites) {
+        int[] indegree = new int[numCourses];
+        Queue<Integer> q = new LinkedList<>();
+        int count = 0;
+        ArrayList<ArrayList<Integer>> adjList = new ArrayList<>();
+        for(int i = 0; i < numCourses; i++) {
+            adjList.add(new ArrayList<>());
+        } 
+
+        for(int i = 0; i < prerequisites.length; i++) {
+            adjList.get(prerequisites[i][0]).add(prerequisites[i][1]);
+        }
+        for(int i = 0; i < numCourses; i++) {
+            for(int adj : adjList.get(i)) {
+                indegree[adj]++;
+            }
+        }
+        for(int i = 0; i < numCourses; i++) {
+            if(indegree[i] == 0) q.add(i);
+        }
+
+        while(!q.isEmpty()) {
+            int node = q.poll();
+            count++;
+            for(int adj : adjList.get(node)) {
+                indegree[adj]--;
+                if(indegree[adj] == 0) {
+                    q.add(adj);
+                }
+            }
+        }
+        if(count == numCourses) 
+            return true;
+        else
+            return false;
+    }
+}
